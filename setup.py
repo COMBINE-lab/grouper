caa55901e388fa9c00c2c5c12ee9bf778ca5ce85
Laraib Iqbal Malik <laraib85@gmail.com>
2017-10-03 14:22:45 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(name='grouper',
       version='0.1.1',
       scripts=['bin/Grouper'],
-      description='Accurate, Fast and Lightweight Clustering of de novo Transcriptomes using Fragment Equivalence Classes',
+      description='Graph-based clustering and annotation for improved de novo transcriptome analysis',
       url='https://github.com/COMBINE-lab/Grouper',
       author='Laraib Malik, Fatemeh Almodaresi, Rob Patro',
       author_email='rob.patro@cs.stonybrook.edu',
